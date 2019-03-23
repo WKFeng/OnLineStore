42902218b4a2a6c4d7f8f385c17a0cfaaeaa9f55
WKFeng <ncepu14jfwk@163.com>
2019-03-23 14:43:27 +0800

    Initial commit

---
+++ src/main/com/store/filter/BaseFilter.java
@@ -0,0 +1,21 @@
+package main.com.store.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "BaseFilter",urlPatterns = "/*")
+public class BaseFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
---
+++ src/main/com/store/utils/JDBCUtil.java
@@ -0,0 +1,4 @@
+package main.com.store.utils;
+
+public class JDBCUtil {
+}
